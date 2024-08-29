d2542b15295fbbb25704822dc0ac136edd9db9a3
Egil Hansen <egil@assimilated.dk>
2024-08-29 17:26:08 +0000

    original import of text renderer from blazor

---
+++ BunitRenderer.cs
@@ -0,0 +1,84 @@
+﻿using AngleSharp;
+using AngleSharp.Dom;
+using AngleSharp.Html.Parser;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.RenderTree;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace AngleSharpExperiments;
+
+public partial class BunitRenderer : Renderer
+{
+    private readonly NavigationManager? _navigationManager;
+    private readonly IBrowsingContext context;
+    private readonly IHtmlParser htmlParser;
+    private IDocument document;
+
+    public BunitRenderer(IServiceProvider serviceProvider, ILoggerFactory loggerFactory) : base(serviceProvider, loggerFactory)
+    {
+        _navigationManager = serviceProvider.GetService<NavigationManager>();
+        var config = Configuration.Default
+            .With<IHtmlParser>(_ => new HtmlParser(new HtmlParserOptions
+            {
+                IsAcceptingCustomElementsEverywhere = true,
+                IsEmbedded = true,
+                IsKeepingSourceReferences = true,
+                IsPreservingAttributeNames = true,
+                OnCreated = (elm, pos) =>
+                {
+                },
+            }));
+
+        context = BrowsingContext.New(config);
+        htmlParser = context.GetService<IHtmlParser>()!;
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            context.Dispose();
+        }
+
+        base.Dispose(disposing);
+    }
+
+    public override Dispatcher Dispatcher { get; } = Dispatcher.CreateDefault();
+
+    public async Task<IDocument> RenderComponentAsync(Type componentType, ParameterView parameters, string domElementSelector)
+    {
+        var component = InstantiateComponent(componentType);
+        var componentId = AssignRootComponentId(component);
+        await RenderRootComponentAsync(componentId, parameters);
+        document = await context.OpenAsync(r => r.Content(""));
+        return document;
+    }
+
+    /// <inheritdoc />
+    protected override Task UpdateDisplayAsync(in RenderBatch batch)
+    {
+        if (document?.Body is { } body)
+        {
+            WriteComponentHtml(0, body);
+        }
+
+        return Task.CompletedTask;
+    }
+
+    /// <inheritdoc />
+    protected override void HandleException(Exception exception)
+    {
+        if (exception is AggregateException aggregateException)
+        {
+            foreach (var innerException in aggregateException.Flatten().InnerExceptions)
+            {
+                Console.WriteLine(innerException.Message);
+            }
+        }
+        else
+        {
+            Console.WriteLine(exception.Message);
+        }
+    }
+}
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,7 +1,7 @@
+using System.Collections;
 using AngleSharp;
 using AngleSharp.Dom;
 using AngleSharp.Html.Parser;
-using AngleSharp.Html.Parser.Tokens.Struct;
 
 namespace AngleSharpExperiments;
 
@@ -10,18 +10,20 @@ public class UnitTest1
     [Fact]
     public async Task Test1()
     {
-        var config = Configuration.Default.With<IHtmlParser>(_ => new HtmlParser(new HtmlParserOptions
-        {
-            IsAcceptingCustomElementsEverywhere = true,
-            IsEmbedded = true,
-            IsKeepingSourceReferences = true,
-            IsPreservingAttributeNames = true,
-            OnCreated = (elm, pos) =>
+        var config = Configuration.Default
+            .With<IHtmlParser>(_ => new HtmlParser(new HtmlParserOptions
             {
-            },
-        }));
+                IsAcceptingCustomElementsEverywhere = true,
+                IsEmbedded = true,
+                IsKeepingSourceReferences = true,
+                IsPreservingAttributeNames = true,
+                OnCreated = (elm, pos) =>
+                {
+                },
+            }));
+
         var context = BrowsingContext.New(config);
-        IDocument document = await context.OpenAsync(r => r.Content("""
+        var raw = """
             <div id='div1'>
                 hi<p>world</p>
                 component 2 content
@@ -30,6 +32,34 @@ public class UnitTest1
                     <span>4 content</span>
                 </p>
             </div>
-            """));
+            """;
+        IDocument document = await context.OpenAsync(r => r.Content(raw));
+
+        var startIndexC2 = raw.IndexOf("component 2 content");
+        var endIndexC2 = startIndexC2 + "component 2 content".Length;
+        var component2Nodes = new NodeListSlice(document.Body!.ChildNodes, startIndexC2, endIndexC2);
+
+    }
+
+    private class NodeListSlice(INodeList nodes, int startIndex, int endIndex) : INodeList
+    {
+        public INode this[int index] => throw new NotImplementedException();
+
+        public int Length { get; }
+
+        public IEnumerator<INode> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ToHtml(TextWriter writer, IMarkupFormatter formatter)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
